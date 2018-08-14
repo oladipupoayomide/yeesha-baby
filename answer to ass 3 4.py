717ef69048795912dcf10a88ab8e423e3300d217
oladipupoayomide <42249213+oladipupoayomide@users.noreply.github.com>
2018-08-14 15:06:09 +0200

    Add files via upload

---
+++ answer to ass 3 4.py
@@ -0,0 +1,27 @@
+Python 3.4.0 (v3.4.0:04f714765c13, Mar 16 2014, 19:24:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+enter the value of pie:3.142
+enter the value of radius:3
+enter the value of theta:30
+area of a sector is
+2.3564999999999996
+enter the length:3
+area of a square is
+9.0
+enter the value for pie:3.142
+enter the value for radius:4
+enter the value for height:5
+area of a cylinder is
+251.35999999999999
+enter value for base:6
+enter value for height:5
+area of a triangle is
+15.0
+enter the value for base 1:2
+enter the value for base 2:8
+enter the value for height:3
+area of a trapezoid is
+15.0
+>>> 
---
+++ assignment 3,4.py
@@ -0,0 +1,72 @@
+"""
+this program is meant to compute the area of a sector
+enter value for pie
+enter value for theta
+enter value for radius
+display  area
+"""
+pie = float(input("enter the value of pie:"))
+radius = float(input("enter the value of radius:"))
+theta= float(input("enter the value of theta:"))
+area= (theta/360)*pie*radius*radius
+print ("area","of","a","sector","is")
+print (area)
+
+"""
+this program is meant to calculate area of a square
+enter value for length
+display area
+"""
+#ask for length
+length = float(input("enter the length:"))
+#multiply to get area
+area =length*length
+#print area
+print ("area","of","a","square","is")
+print (area)
+
+"""
+this program is expected to calculate the area of a cylinder
+enter the value of radius
+enter the value of height
+enter the value of pie
+display area
+"""
+pie = float(input("enter the value for pie:"))
+radius = float(input("enter the value for radius:"))
+height = float(input("enter the value for height:"))
+area = (pie*radius*radius*height)
+print ("area","of","a","cylinder","is")
+print(area)
+
+"""
+this program is meant to calculate the area of a triangle
+you are required to
+enter value for base
+enter value for height
+display area
+"""
+base = int(input("enter value for base:"))
+height = int(input("enter value for height:"))
+area =(1/2)*base*height
+print ("area","of","a","triangle","is")
+print (area)
+
+"""
+this program is meant to compute area of a trapezoid
+you are required to
+enter the value for base 1
+enter the value for base 2
+enter the value for height
+display area
+"""
+base1=float(input("enter the value for base 1:"))
+base2=float(input("enter the value for base 2:"))
+height=float(input("enter the value for height:"))
+area=((base1+base2)*height)/2
+print("area","of","a","trapezoid","is")
+print(area)
+
+        
+               
+
