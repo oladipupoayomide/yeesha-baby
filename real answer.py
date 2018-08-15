dd440c8795340a450b6975703f6986b84cf4814e
oladipupoayomide <42249213+oladipupoayomide@users.noreply.github.com>
2018-08-15 14:14:20 +0200

    i just added new files

---
+++ real answer.py
@@ -0,0 +1,32 @@
+Python 3.4.0 (v3.4.0:04f714765c13, Mar 16 2014, 19:24:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+this program is meant to calculate area of a sector
+enter the value of pie:3.142
+enter the value of radius:4
+enter the value of theta:30
+area of a sector is
+4.189333333333333
+this program is meant to calculate area of a square
+enter the length:5
+area of a square is
+25.0
+this is expected to calculate the area of a cylinder
+enter the value for pie:3.142
+enter the value for radius:4
+enter the value for height:6
+area of a cylinder is
+301.632
+this program is meant to calculate area of a triangle
+enter value for base:5
+enter value for height:8
+area of a triangle is
+20.0
+this program is meant to calculate area of a trapezoid
+enter the value for base 1:4
+enter the value for base 2:7
+enter the value for height:9
+area of a trapezoid is
+49.5
+>>> 
